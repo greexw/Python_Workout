d4f28a7795094c945fcac25bcdc25a9edfd26180
greexw <mateusz.plonka1998@gmail.com>
2022-02-01 18:15:37 +0100

    Reversed Strings

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-#Build a function that returns an array of integers from n to 1 where n>0.
-#Example : n=5 --> [5,4,3,2,1]
+#Reversed Strings
 
+def solution(string):
+    return string[::-1]
 
-def reverse_seq(n):
-    return list(range(n, 0, -1))
-
-print(reverse_seq(100))
+print(solution('world'))
