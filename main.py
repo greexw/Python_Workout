f0671a66c708b38affe9410f1c732ceb2077eb39
greexw <mateusz.plonka1998@gmail.com>
2022-02-08 18:33:22 +0100

    Guessing game - U.S.A States created by Turtle and Pandas

--- main.py
+++ main.py
@@ -1,39 +1,32 @@
-import time
-from turtle import Screen
-from player import Player
-from car_manager import CarManager
-from scoreboard import Scoreboard
+import pandas
+import turtle
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.tracer(0)
-screen.title("The Turtle Crossing")
+screen = turtle.Screen()
+screen.title("U.S. States")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
 
-player = Player()
-car_manager = CarManager()
-scoreboard = Scoreboard()
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+while len(guessed_states) < len(all_states):
+    answer_state = screen.textinput(title=f"States: {len(guessed_states)}/{len(all_states)}",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+        guessed_states.append(answer_state)
 
-screen.listen()
-screen.onkey(player.move, "Up")
-
-level = 1
-
-game_is_on = True
-while game_is_on:
-    time.sleep(0.1)
-    screen.update()
-
-    if player.check_win_status():
-        level += 1
-        scoreboard.refresh_scoreboard(level)
-        player.start_pos()
-
-    car_manager.create_car()
-    car_manager.move_cars(level)
-
-    for car in car_manager.all_cars:
-        if car.distance(player.player) < 15:
-            game_is_on = False
-            scoreboard.game_over()
-
-screen.exitonclick()
