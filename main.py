be116f110d27e5fec680f41a239d9e2bd16cb3f1
greexw <mateusz.plonka1998@gmail.com>
2022-02-13 15:49:35 +0100

    ISS position checker with e-mail reminder if close to us
    Created by requests and smtplib libraries.

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
+import requests
+from datetime import datetime
 import smtplib
-import datetime as dt
-import pandas
-import random
+import time
 
-current_date = dt.datetime.now()
-current_day = current_date.day
-current_month = current_date.month
-
-letters = []
-for i in range(1, 4):
-    with open(f"letter_templates/letter_{i}.txt") as file:
-        letters.append(file.read())
+MY_LAT = 51.126454
+MY_LONG = -28.41
 
 
-def send_wishes(name, email):
-    wishes = random.choice(letters).replace("[NAME]", name)
+def check_position():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    iss_longitude = float(response.json()["iss_position"]["longitude"])
+    iss_latitude = float(response.json()["iss_position"]["latitude"])
 
-    password = "TEST321test"
-    my_email = "test123test4568@gmail.com"
-
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=my_email, password=password)
-        connection.sendmail(from_addr=my_email, to_addrs=email,
-                            msg=f"Subject:Happy Birthday!\n\n{wishes}")
+    if (MY_LONG + 5 > iss_longitude > MY_LONG - 5) and (MY_LAT + 5 > iss_latitude > MY_LAT - 5):
+        return True
+    else:
+        return False
 
 
-data = pandas.read_csv("birthdays.csv")
-for index, row in data.iterrows():
-    if current_day == row["day"] and current_month == row["month"]:
-        send_wishes(row["name"], row["email"])
+def check_is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    if datetime.now().hour < sunrise or datetime.now().hour > sunset:
+        return True
+    else:
+        return False
+
+
+while True:
+    time.sleep(60)
+    if check_is_night() and check_position():
+        password = "TEST321test"
+        my_email = "test123test4568@gmail.com"
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email, to_addrs=email,
+                                msg=f"Subject:ISS is close to you!\n\n "
+                                    f"Hi! Look out the windows! ISS is close to you!")
+    else:
+        print("ISS is far far away")
