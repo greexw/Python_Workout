886a9c944b63ceb38f9a60fc19706b16c7e2d18c
greexw <mateusz.plonka1998@gmail.com>
2022-03-03 20:08:51 +0100

    Stock price checker + company news printer (by API)
    This app using the alphavantage and newsapi APIs to check te change of the stock price of company (in this code that is Apple Inc). If the price was changed by 1% or more - the app will print the 3 newest articles about company.

--- main.py
+++ main.py
@@ -1,65 +1,42 @@
 import requests
-from twilio.rest import Client
+import datetime
 
-API_KEY = ""
-OWM_Endpoint = ""
-ACCOUNT_SID = ""
-AUTH_TOKEN = ""
+STOCK = "AAPL"
+COMPANY_NAME = "Apple Inc"
+AH_APIKEY = ""
+AH_API_URL = "https://www.alphavantage.co/query?"
+NA_APIKEY = ""
+NA_API_URL = "https://newsapi.org/v2/everything"
 
-weather_params = {
-    "lat": 51.50,
-    "lon": -0.5,
-    "appid": API_KEY,
-    "exclude": "current,minutely,daily",
+ah_parameters = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "apikey": AH_APIKEY,
 }
 
-will_rain = False
+# get the company stock info from alphavantage API
+ah_response = requests.get(AH_API_URL, params=ah_parameters)
+data = ah_response.json()['Time Series (Daily)']
 
-response = requests.get(OWM_Endpoint, params=weather_params)
-response.raise_for_status()
-weather_data = response.json()
-weather_slice = weather_data["hourly"][:12]
-for hour_data in weather_slice:
-    condition_code = hour_data["weather"][0]["id"]
-    if condition_code < 700:
-        will_rain = True
+yesterday = datetime.date.today() - datetime.timedelta(days=1)
+day_before_yesterday = yesterday - datetime.timedelta(days=1)
 
-if will_rain:
-    client = Client(ACCOUNT_SID, AUTH_TOKEN)
-    message = client.messages \
-       .create(
-       body="It's goint to rain today! Bring umbrella with you.",
-       from_='',
-       to=''
-    )
-    print(message.status)
+yesterday_close_price = float(data[str(yesterday)]['4. close'])
+db_yesterday_close_price = float(data[str(day_before_yesterday)]['4. close'])
 
+na_parameters = {
+    "apiKey": NA_APIKEY,
+    "q": COMPANY_NAME
+}
 
+# get the news about company from newsapi API
+na_response = requests.get(NA_API_URL, params=na_parameters)  # get the news about our company
+na_data = na_response.json()["articles"]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# if the close price was changed by 1% or more - print the 3 newest articles about company
+if (yesterday_close_price-db_yesterday_close_price)/db_yesterday_close_price >= 0.01:
+    print(f"{COMPANY_NAME} price has changed by minimum 1%! (from {db_yesterday_close_price}$ to {yesterday_close_price}$)."
+          f" Here are some news about your company: \n")
+    for i in range(3):
+        print(na_data[i]['title'])
+        print(na_data[i]['description']+"\n")
